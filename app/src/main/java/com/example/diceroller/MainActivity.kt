63aabca44364870a744adcaa0db1ea159ae1c86f
ChinXin <68983548+ChinXin@users.noreply.github.com>
2021-06-30 14:12:44 +0800

    Dice Roller Image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,48 +3,36 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnRoll : Button = findViewById(R.id.roll_button)
-        val btnCountUp : Button = findViewById(R.id.countUp_button)
 
-        btnRoll.setOnClickListener() {
-            rollDice()
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+        btnRoll.setOnClickListener {
+            diceImage1.setImageResource(getRandomDiceImage())
+            diceImage2.setImageResource(getRandomDiceImage())
         }
-
-        btnCountUp.setOnClickListener() {
-            countUp()
-        }
-
     }
 
-    private fun rollDice() {
-        val randomInt : Int = (1..6).random()
-
-        val tvResult : TextView = findViewById(R.id.result_text)
-        tvResult.text = randomInt.toString()
-    }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        }
-        else
-        {
-            var resultInt = resultText.text.toString().toInt()
-
-            if(resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+    private fun getRandomDiceImage(): Int {
+        return when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 }
